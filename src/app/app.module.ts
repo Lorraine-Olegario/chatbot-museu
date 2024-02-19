98581eabf61da610fb5403f1bcea50f9ad06a2aa
Lorraine-Olegario <lorraineolegario16@gamil.com>
2024-02-18 22:01:20 -0300

    feat: add initial logic to open chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ArrowRightComponent } from './icons/arrow-right/arrow-right.component';
 import { SparkleComponent } from './icons/sparkle/sparkle.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
+        ChatComponent,
     ],
     imports: [
         BrowserModule,
